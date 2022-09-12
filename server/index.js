96ca1d728dfcf77d8df769feb1b4a83bb30519ad
YouRain <rustam.men@mail.ru>
2022-09-12 23:11:58 +0400

    firstcommit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 8080
+app.use(cors());
 
 app.get('/api/users', (req, res) => {
   res.send(
@@ -11,3 +13,9 @@ app.get('/api/users', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+let corsOptions = {
+  origin : ['http://localhost:3000/'],
+}
+// app.use(cors());
